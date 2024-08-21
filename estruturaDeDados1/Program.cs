894f974d72f35581c33119c61bd02ef8c80da730
ThiagoMZZ <thiagodev728@gmail.com>
2024-08-21 10:38:32 -0300

    Trabalhando com o branch, teste 2.

--- estruturaDeDados1/Program.cs
+++ estruturaDeDados1/Program.cs
@@ -3,4 +3,6 @@
 static void NewMethod()
 {
     Console.WriteLine("My first branch");
+    Console.WriteLine("Working with Git\\Github");
+    Console.WriteLine("Mais uma tentativa de trabalhar com o git");
 }
